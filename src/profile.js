9c022c5cb39ddb26ad2e386f23e5342b803d043c
Ntshangase <simphiwe323i@gmail.com>
2022-11-03 21:55:28 +0200

    stage 1

--- src/profile.js
+++ src/profile.js
@@ -12,14 +12,14 @@ export default function Profile() {
         <img id="profile__img" src={logo} alt="profile__img"/>
         <p> <h1 id='slack'> Zulu</h1> </p>
         <p>
-        <a id='twitter' href='https://twitter.com/elonmusk' target="blank">@elonmusk</a>
+        <a id='twitter' href='https://twitter.com/elonmusk' target="blank"><h4>Twitter Link</h4></a>
         </p>
         <p> 
-          <p> <a id='btn__zuri' href='https://training.zuri.team/' target="blank">Zuri training</a> </p> 
-          <p> <a id='books' href='http://books.zuri.team/' target="blank"> <h4>Zuri Bookstore</h4> <p>Get books about design and coding</p></a> </p>
-          <p> <a id='book__python' href='http://books.zuri.team/python-for-beginners?ref_id=Zulu' target="blank"><h4>Check out</h4> <p>Basic coding for you! </p></a> </p>
-          <p> <a id='pitch' href='https://background.zuri.team/' target="blank"><h4>background check</h4> <p>Let's clear that background so you can get that job</p></a> </p>
-          <p> <a id='book__design' href='https://books.zuri.team/design-rules' target="blank"><h4>Zuri freebooks</h4> <p>The all you can Read book buffet, for free!!!</p></a> </p>
+          <p> <a id='btn__zuri' href='https://training.zuri.team/' target="blank"> <h4>Zuri team</h4></a> </p> 
+          <p> <a id='books' href='http://books.zuri.team/' target="blank"> <h4>Zuri Book</h4> <p>Get books about design and coding</p></a> </p>
+          <p> <a id='book__python' href='http://books.zuri.team/python-for-beginners?ref_id=Zulu' target="blank"><h4>Python Books</h4> <p>Basic coding for you! </p></a> </p>
+          <p> <a id='pitch' href='https://background.zuri.team/' target="blank"><h4>Background Check for Coders</h4> <p>Let's clear that background so you can get that job</p></a> </p>
+          <p> <a id='book__design' href='https://books.zuri.team/design-rules' target="blank"><h4>Design Books</h4> <p>The all you can Read book buffet, for free!!!</p></a> </p>
         </p>
       </center>
     </div>
