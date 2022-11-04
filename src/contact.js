2e3d5017db926abfcdb8cf377f5534b8ccfacf64
Ntshangase <simphiwe323i@gmail.com>
2022-11-05 01:57:38 +0200

    contact page update

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 
 
-export default function contactPage () {
-    return (
-        <div>
-        <p>This page works</p>
-        </div>
-    )
+export default function contactPage() {
+  return (
+    <Form>
+        
+        <Col>
+        <Form.Label>First Name</Form.Label>
+          <Form.Control placeholder="First name" />
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control placeholder="Last name" />
+        </Col>
+      
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
