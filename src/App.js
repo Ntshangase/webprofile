0def90a0185257f9495eddf24f5119cbaa503620
Ntshangase <simphiwe323i@gmail.com>
2022-11-04 14:25:26 +0200

    router-dom installed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import React from "react";
 import Profile from "./profile";
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {tittle: 'contacts', path: "/contact"}
+  }
+
+
   render () {
     return (
       <>
